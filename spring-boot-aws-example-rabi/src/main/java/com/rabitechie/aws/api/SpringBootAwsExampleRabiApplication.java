ddeaced2027252fb51b800c35d7e882cfb4fbd90
rabi0 <rabi0@LAPTOP-3PARCRTL>
2020-07-13 14:29:10 +0530

    Spring Boot AWS!!!

---
+++ spring-boot-aws-example-rabi/src/main/java/com/rabitechie/aws/api/SpringBootAwsExampleRabiApplication.java
@@ -0,0 +1,13 @@
+package com.rabitechie.aws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsExampleRabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsExampleRabiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aws-example-rabi/src/test/java/com/rabitechie/aws/api/SpringBootAwsExampleRabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabitechie.aws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsExampleRabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
